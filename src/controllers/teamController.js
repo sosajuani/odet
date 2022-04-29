163818b2b8ea673e6eab6e9b7090569a4327a457
sosajuani <sosajuanignacio17@gmail.com>
2022-04-29 17:17:55 -0300

    teams rediseño, paginacion, sass, arreglo de bugs

--- src/controllers/teamController.js
+++ src/controllers/teamController.js
@@ -5,16 +5,84 @@ const Division = db.Division;
 
 const teamController = {
     teams: async(req,res)=>{
-        try{
-            const tournamentConsult = await Tournament.findAll();
-            const teamsConsult = await Team.findAll();
-            const divisionConsult = await Division.findAll();
-            const tournamentId = null;
-            const divisionId = null;
-            res.render('userViews/pages/teams/teamsMain.ejs',{teamsConsult,tournamentConsult,divisionConsult,tournamentId,divisionId});
-        }catch(e){
-            console.log(e);
+        const tournamentConsult = await Tournament.findAll();
+        const countTeams = await Team.count();
+        const divisionConsult = await Division.findAll();
+        const tournamentId = null;
+        const divisionId = null;
+        
+        let pages;
+        let pagesCount=0;
+        const limit= 20;
+        if(countTeams > limit){
+            pages = true;
+            pagesCount = Math.ceil(countTeams / limit)
+        }else{
+            pages = false
         }
+        let teamsConsult;
+        let pageQuery = parseInt(req.query.page);
+        let offset;
+        if(pageQuery){
+            pageQuery > pagesCount ? res.redirect("/teams") : null
+            offset= (pageQuery - 1)* limit
+            teamsConsult = await Team.findAll({
+                limit: limit,  
+                offset: offset,
+                include: ['avatars']
+            })
+        }else{
+            teamsConsult = await Team.findAll({
+                limit: limit,
+                include: ['avatars']
+            })
+        }
+        
+        res.render('userViews/pages/teams/teamsMain.ejs',{teamsConsult,pages,pagesCount,pageQuery,tournamentConsult,divisionConsult,tournamentId,divisionId});
+    },
+    teamsFilter: async(req,res)=>{
+        const tournamentId = req.query.tournamentId;
+        const divisionId = req.query.divisionId;
+        
+        const tournamentConsult = await Tournament.findAll();
+        const divisionConsult = await Division.findAll();        
+        // const countTeams = await Team.count({
+        //     where: {tournamentId,divisionId}
+        // });
+        // let pages;
+        // let pagesCount=0;
+        // const limit= 20;
+        // if(countTeams > limit){
+        //     pages = true;
+        //     pagesCount = Math.ceil(countTeams / limit)
+        // }else{
+        //     pages = false
+        // }
+        // let teamsConsult;
+        // let pageQuery = parseInt(req.query.page);
+        // let offset;
+        // if(pageQuery){
+        //     pageQuery > pagesCount ? res.redirect("/teams") : null
+        //     offset= (pageQuery - 1)* limit
+        //     teamsConsult = await Team.findAll({
+        //         limit: limit,  
+        //         offset: offset,
+        //         include: ['avatars']
+        //     })
+        // }else{
+        //     teamsConsult = await Team.findAll({
+        //         limit: limit,
+        //         include: ['avatars']
+        //     })
+        // }
+        const teamsConsult = await Team.findAll({
+            where: {
+                tournamentId,
+                divisionId
+            },
+            include: ['avatars']
+        })
+        res.render('userViews/pages/teams/teamsFilter.ejs',{teamsConsult,tournamentConsult,divisionConsult,tournamentId,divisionId});
     },
     teamsDetail: async(req,res)=>{
         let teamConsult = await Team.findByPk(req.params.id,{
@@ -53,7 +121,6 @@ const teamController = {
         if(teamConsult == null){
             res.render('404.ejs');
         }else{
-            
             res.render('pages/teams/teamsDetailMore.ejs',{teamConsult})
         }
     }
--- src/routes/teamRoutes.js
+++ src/routes/teamRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const teamController = require('../controllers/teamController');
 
 router.get('/',teamController.teams)
+router.get('/filter',teamController.teamsFilter)
 router.get('/:id/info',teamController.teamsDetail)
 router.get('/:id/position',teamController.teamsDetailPosition)
 router.get('/:id/players',teamController.teamsDetailPlayer)
